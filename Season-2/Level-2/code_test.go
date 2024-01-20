be56705d602ceda7753c744e4a7c3392b00bc391
Fahmi Fahreza <78774559+Man4ct@users.noreply.github.com>
2024-01-20 22:02:04 +0000

    s2-lv2

--- Season-2/Level-2/code_test.go
+++ Season-2/Level-2/code_test.go
@@ -7,10 +7,11 @@
 
 // If 'go' is not found when running the above, install it from:
 // https://go.dev/dl/
-
 package main
 
 import (
+	"bytes"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -18,6 +19,80 @@ import (
 	"testing"
 	"time"
 )
+
+func TestLoginHandler_UserEnumeration_InvalidEmail(t *testing.T) {
+	reqBody := `{"email": "invalid@example.com", "password": "password12345"}`
+	req, err := http.NewRequest("POST", "/login", strings.NewReader(reqBody))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(loginHandler)
+	handler.ServeHTTP(recorder, req)
+
+	if recorder.Code != http.StatusUnauthorized {
+		t.Errorf("Expected status code %d, but got %d", http.StatusUnauthorized, recorder.Code)
+	}
+
+	respBody := strings.TrimSpace(recorder.Body.String())
+	expectedRespBody := "Invalid Email or Password"
+	if respBody != expectedRespBody {
+		t.Errorf("Expected body %q, but got %q", expectedRespBody, respBody)
+	}
+}
+
+func TestLoginHandler_SensitiveLog_InvalidEmail_FIXME(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+
+	reqBody := `{"email": "in.valid@ex@mple.com", "password": "password12345"}`
+	req, err := http.NewRequest("POST", "/login", strings.NewReader(reqBody))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(loginHandler)
+	handler.ServeHTTP(recorder, req)
+
+	if recorder.Code != http.StatusBadRequest {
+		t.Errorf("Expected status code %d, but got %d", http.StatusBadRequest, recorder.Code)
+	}
+
+	logOutput := buf.String()
+	expectedRespLog := "Invalid email format"
+	if strings.Contains(logOutput, "in.valid@ex@mple.com") {
+		t.Errorf("Expected body %q, but got %q", expectedRespLog, logOutput)
+		t.Fail()
+	}
+}
+
+func TestLoginHandler_SensitiveLog_ValidCredentials_FIXME(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
+
+	reqBody := `{"email": "user1@example.com", "password": "password12345"}`
+	req, err := http.NewRequest("POST", "/login", strings.NewReader(reqBody))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+
+	recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(loginHandler)
+	handler.ServeHTTP(recorder, req)
+
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Expected status code %d, but got %d", http.StatusOK, recorder.Code)
+	}
+
+	logOutput := buf.String()
+	expectedRespLog := "Successful login request"
+	if strings.Contains(logOutput, "user1@example.com") || strings.Contains(logOutput, "password12345") {
+		t.Errorf("Expected body %q, but got %q", expectedRespLog, logOutput)
+		t.Fail()
+	}
+}
 
 func TestLoginHandler_ValidCredentials(t *testing.T) {
 	reqBody := `{"email": "user1@example.com", "password": "password12345"}`
